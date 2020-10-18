8628511e434a4df592523887769b3e117158b9e8
Daniel Pires <danielpires6@hotmail.com>
2020-10-18 17:37:23 -0300

    Mudanças na arquitetura do projeto modularizando algumas pastas

---
+++ src/data/model.js
@@ -0,0 +1,18 @@
+// Modelo de dados
+const model = {
+  "codigo": 1719,
+  "titulo": "ARTE",
+  "subtitulo": "",
+  "informacao": "Cartão, medindo 14 x 18 cm, produzido nos anos de 80. CARLOS ECHEVERRY",
+  "unidade": "Núcleo de Arte e Cultura",
+  "estado_conservacao": "BOM",
+  "situacao": "EM_ACERVO",
+  "numero_tombamento": "",
+  "colecao": "Arte Correio",
+  "subcolecao": "",
+  "tipologia": "Cartão",
+  "cedido_a": "",
+  "data_limite_cessao": ""
+}
+
+module.exports = model;
---
+++ src/script.js
@@ -0,0 +1,46 @@
+const writeSQLFile = (file, insertIntoSQL) => {
+  var fs = require('fs');
+  var stream = fs.createWriteStream(`tables/${file}.sql`);
+  stream.once('open', function(fd) {
+    stream.write(insertIntoSQL);
+    stream.write("\n");
+    stream.end();
+  });
+}
+
+let dataInJSON = require("./data/dadosJSON.js");
+
+// Criar Tabela de Estado de Conservação
+const todos_estado_conservacao = dataInJSON.map(obj => {
+  return obj.estado_conservacao
+});
+
+const estado_conservacao = [...new Set(todos_estado_conservacao)];
+
+const obj_estado_conservacao = estado_conservacao.map((estado, index) => {
+  return {
+      index: index,
+      estado: estado
+  }
+})
+
+let insert_into_BD = obj_estado_conservacao.reduce(function(acumulador, valorAtual) {
+  return acumulador + `INSERT INTO ESTADO_DE_CONSERVACAO (COD_ESTADO, NOME_ESTADO) VALUES (${valorAtual.index}, "${valorAtual.estado}"); \n`;
+}, '');
+
+writeSQLFile('estadoDeConservacao',insert_into_BD);
+
+
+// Objetos Históricos
+const todos_objetos_historicos = dataInJSON.map(obj => {
+  return ({
+      titulo: obj.titulo,
+      codigo: obj.codigo
+  })
+});
+
+insert_into_BD = todos_objetos_historicos.reduce(function(acumulador, valorAtual) {
+  return acumulador + `INSERT INTO OBJETOS_HISTORICOS (CODIGO, TITULO) VALUES (${valorAtual.codigo}, "${valorAtual.titulo}"); \n`;
+}, '');
+
+writeSQLFile('objetosHistoricos', insert_into_BD);
